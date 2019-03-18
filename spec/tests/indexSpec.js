623dc761737526d9a85243e6bbc8753d0dd6fbf0
partho <partho.sarathi@globalids.com>
2019-03-18 17:00:09 +0530

    Added unit test

---
+++ spec/tests/indexSpec.js
@@ -0,0 +1,8 @@
+describe("Algorithms", function() {
+    it("Problem 1", function() {
+      const algo = require("../../src/algo/problem1");
+      var result = algo();
+
+      expect(result).toBe(233168);
+    });
+  });
